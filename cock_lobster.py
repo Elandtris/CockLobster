2db24c279b20abdffda3ba3ee8b3ec3310b5ee4c
bazir21 <bayir@live.ie>
2020-12-14 00:12:08 +0000

    implemented basic movement and jumps along with proper mouse input support, birds draw to screen

--- cock_lobster.py
+++ cock_lobster.py
@@ -6,12 +6,15 @@ import random
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
+mouse_pressed = False
+
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Cock Lobster")
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird" + str(x) + ".png"))) for x in range(1,4)]
-PIPE_IMG = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))]
-BASE_IMG = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))]
+#BIRD_IMGS = [pygame.transform.scale(pygame.image.load(os.path.join("imgs","cocklob1.png")), (200, 200)), pygame.transform.scale(pygame.image.load(os.path.join("imgs","cocklob2.png")), (200, 200)), pygame.transform.scale(pygame.image.load(os.path.join("imgs","cocklob3.png")), (200, 200))]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale(pygame.image.load(os.path.join("imgs","bg.png")).convert_alpha(), (600, 900))
 
 class Bird:
@@ -25,26 +28,26 @@ class Bird:
 		self.y = y
 		self.tilt = 0
 		self.tick_count = 0
-		self.vol = 0
+		self.vel = 0
 		self.height = self.y
 		self.img_count = 0
 		self.img = self.IMGS[0]
 
 	def jump(self):
-		self.vel = -10.5
+		self.vel = -0.1
 		self.tick_count = 0
 		self.height = self.y
 
 	def move(self):
-		self.tick_count += 1
+		self.tick_count += 0.0005
 
-		d = self.vel*self.tick_count + 1.5*self.tick_count**2
+		d = self.vel * self.tick_count + 1.5 * self.tick_count**2
 
 		if d >= 16:
 			d = 16
 
 		if d < 0:
-			d -= 2
+			d -= 1
 
 		self.y = self.y + d
 
@@ -55,6 +58,12 @@ class Bird:
 		else:
 			if self.tilt > -90:
 				self.tilt -= self.ROT_VOL
+
+		if self.y > WIN_HEIGHT:
+			self.y = WIN_HEIGHT - 48
+
+		if self.y < 0:
+			self.y = 0
 
 
 	def draw(self, win):
@@ -76,14 +85,32 @@ class Bird:
 			self.img = self.IMGS[1]
 			self.img_count = self.ANIMATION_TIME*2
 
+		blitRotateCenter(win, self.img, (self.x, self.y), self.tilt)
+
 	def get_mask(self):
 		return pygame.mask.from_surface(self.img)
 
+def blitRotateCenter(surf, image, topleft, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center = image.get_rect(topleft = topleft).center)
+
+    surf.blit(rotated_image, new_rect.topleft)
 
 def draw_window(win, bird):
 	win.blit(BG_IMG, (0,0))
 	bird.draw(win)
 	pygame.display.update()
+
+def mouse_clicked(event):
+	global mouse_pressed
+	if event == (0, 0, 0):
+		mouse_pressed = False
+		return False
+	elif mouse_pressed:
+		return False
+	elif event == (1, 0, 0):
+		mouse_pressed = True
+		return True
 
 
 def main():
@@ -96,6 +123,12 @@ def main():
 				run = False
 				pygame.quit()
 				quit()
+
+		bird.move()
+
+		if mouse_clicked(pygame.mouse.get_pressed()):
+			print('mf jump')
+			bird.jump()
 
 		draw_window(WIN, bird)
 		
